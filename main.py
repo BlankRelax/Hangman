b3785b63004da3baa76bc1af0333eb0cf50a1dd9
Hassaan Mirza <84412764+BlankRelax@users.noreply.github.com>
2022-11-22 11:23:25 +0000

    hangman

--- main.py
+++ main.py
@@ -8,7 +8,20 @@ for i in range(0, len(word_to_guess)):
      guess_array.append("*")
 print(guess_array)
 lives = 10
-while lives <11:
+letters_guessed = 0
+while lives >0:
     letter_guess = input("Enter a letter to guess")
+    for i in range(0, len(word_to_guess_a)):
+        if word_to_guess_a[i] == letter_guess:
+            guess_array[i] = letter_guess
+            letters_guessed+=1
+    print(guess_array)
+    lives -= 1
+    if letters_guessed == len(word_to_guess_a):
+        print("Well done, you guessed within your life span!")
+        break
+if lives <=0:
+    print("The word was: " + word_to_guess)
+    print("You got hanged")
 
 
